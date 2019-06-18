f347d6cc604463f3a020c0a3709fb1f397f425e8
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 00:35:01 -0700

    finish section 2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,27 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const copyCollection = [...collection];
+
+  collection.forEach((el, index) => {
+    const element = el.split('-');
+    if (element.length > 1) {
+      const newArr = [];
+      for (let i=0; i < element[1]; i++) {
+        newArr.push(element[0]);
+      }
+      copyCollection.splice(index, 1, newArr)
+    }
+  });
+
+  const baseCollection = copyCollection.flat();
+  const keyCollection = new Set(baseCollection);
+
+  return Array.from(keyCollection).map(el => {
+    const count = baseCollection.reduce((total, x) => (x === el ? total+1 : total), 0);
+    return {
+      key: el,
+      count,
+    };
+  });
 }
--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,28 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const copyCollection = [...collection];
+
+  collection.forEach((el, index) => {
+    if (el.split('').length > 1) {
+      const newArr = [];
+      const count = el.replace(/[^0-9]/ig,"");
+      const letter = el.replace(/[^a-z]/ig,"");
+      for (let i=0; i < count; i++) {
+        newArr.push(letter);
+      }
+      copyCollection.splice(index, 1, newArr)
+    }
+  });
+
+  const baseCollection = copyCollection.flat();
+  const keyCollection = new Set(baseCollection);
+
+  return Array.from(keyCollection).map(el => {
+    const summary = baseCollection.reduce((total, x) => (x === el ? total+1 : total), 0);
+    return {
+      name: el,
+      summary,
+    };
+  });
 }
